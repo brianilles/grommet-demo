21bad6883e9ba8f23c550fcfd0ff66fbb642c510
Brian Illes <brianrilles@gmail.com>
2019-04-27 07:28:30 -0700

    Adds Grommet them and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Box, Grommet } from "grommet";
+
+const AppBar = props => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    elevation="medium"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    style={{ zIndex: "1" }}
+    {...props}
+  />
+);
+
+const theme = {
+  global: {
+    colors: {
+      brand: "lightgray"
+    },
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "15px"
+    }
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <p>sanity</p>
-    </div>
+    <Grommet theme={theme}>
+      <AppBar>Hello World from Grommet</AppBar>
+    </Grommet>
   );
 }
 
